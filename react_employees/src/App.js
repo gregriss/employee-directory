21c0c347dc27c86b7b42625e6083f1804749a1a1
Greg Riss <gregriss23@gmail.com>
2021-04-25 18:55:47 -0400

    add person icon to app, refactor app.js

--- react_employees/src/App.js
+++ react_employees/src/App.js
@@ -1,22 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
+// import logo from './logo.svg';
+import person from './person-icon.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
+        <img src={person} className="App-logo" alt="logo" />
+        <h1>
+          Employee Directory 
+          {/* <code>src/App.js</code> */}
+        </h1>
+        {/* <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a> */}
       </header>
     </div>
   );
