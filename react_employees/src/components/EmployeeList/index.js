d2c0b9615e0d53d0164c6bc5c2b4786ee562920e
Greg Riss <gregriss23@gmail.com>
2021-04-27 17:28:04 -0400

    add styles to each result li

--- react_employees/src/components/EmployeeList/index.js
+++ react_employees/src/components/EmployeeList/index.js
@@ -1,14 +1,26 @@
 import React from "react";
+// import EmployeeCard from "../employeeCard";
 
 const styles = {
+  ul: {
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr)",
+    gap: "1rem"
+  },
+
   li: {
-    listStyle: "none"
+    listStyle: "none",
+    display: "inline",
+    padding: "1rem",
+    boxShadow: "1px 1px 8px 5px #EEEEEE"
   }
 }
+
 function EmployeeList(props) {
   return (
-    <ul className="list-group">
+    <ul style={styles.ul} className="list-group">
       {props.results.map(result => (
+        // <EmployeeCard/>
         <li style={styles.li}className="list-group-item" key={result.login.uuid}>
           <img alt={result.name.first.last} className="img-fluid" src={result.picture.thumbnail} />
           <p>{result.name.first} {result.name.last}</p>
---
+++ react_employees/src/components/employeeCard/index.js
@@ -0,0 +1,17 @@
+// import React from 'react';
+// import EmployeeList from '../employeeList';
+// import SearchForm from '../searchForm';
+// import api from "../../utils/api";
+
+// function EmployeeCard (props) {
+//     return (
+//     <li style={styles.li}className="list-group-item" key={result.login.uuid}>
+//     <img alt={result.name.first.last} className="img-fluid" src={result.picture.thumbnail} />
+//     <p>{result.name.first} {result.name.last}</p>
+//     <p>{result.email}</p>
+//     <p>Location: {result.location.city}, {result.location.country}</p>
+//   </li>
+//     )
+// }
+
+// export default EmployeeCard;
